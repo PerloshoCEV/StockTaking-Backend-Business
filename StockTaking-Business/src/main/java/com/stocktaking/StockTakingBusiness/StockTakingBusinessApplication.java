dbe0852ed90b377c99bea4c4adadd14e9fd26ee3
Diogenes Alcina <127620996+PerloshoCEV@users.noreply.github.com>
2023-04-22 15:43:44 +0200

    Init Project
    Proyecto maven en blanco

---
+++ StockTaking-Business/src/main/java/com/stocktaking/StockTakingBusiness/StockTakingBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.stocktaking.StockTakingBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTakingBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTakingBusinessApplication.class, args);
+	}
+
+}
